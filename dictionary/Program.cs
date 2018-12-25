550774322f0a483a28f24698d6c11d8d50dea856
Joy Trinh <mongtuyen149@gmail.com>
2018-12-25 17:40:45 +0700

    Add word into a dictionary with hash tables

--- dictionary/Program.cs
+++ dictionary/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace dictionary
 {
@@ -10,7 +11,32 @@ namespace dictionary
     {
         static void Main(string[] args)
         {
+            Hashtable dict = new Hashtable();
+            string word;
+            int key = 0;
+            do
+            {
+                Console.WriteLine("Add a word: ");
+                word = Console.ReadLine();
+                byte[] bytes = Encoding.ASCII.GetBytes(word);
 
+                dict.Add(key, word);
+            } while (word != "-1");
+            ICollection keys = dict.Keys;
+            foreach (string k in keys)
+            {
+                Console.WriteLine(dict[k]);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Node
+    {
+        public Node next = null;
+        public object data;
+        public Node(object val)
+        {
+            data = val;
         }
     }
 }
