f7d418f445bf63b7c188c7afbe5ec4d27f6b3d5c
Joy Trinh <mongtuyen149@gmail.com>
2018-12-26 22:37:01 +0700

    Insert new words with LinkedList and HashTable. Then print all words.

--- dictionary/Program.cs
+++ dictionary/Program.cs
@@ -13,41 +13,110 @@ namespace dictionary
     {
         static void Main(string[] args)
         {
-            Hashtable dict = new Hashtable();
-
-            //1. Calls load on the dictionary file
-            string word = "";
+            int size = 26;
+            FixedSizeHashTable hash = new FixedSizeHashTable(size);
+            string word = "abc";
             while (word != " ")
             {
-                Console.WriteLine("Add a word: ");
-                word = Console.ReadLine().ToLower().Trim();//Lower all letters and trim irrelevant spaces
-
-                //Dictionary contains valid words
-                bool success = Regex.IsMatch(word, @"^[a-zA-Z]\[']+$");
+                Console.WriteLine("Please input a word: ");
+                word = Console.ReadLine().Trim().ToLower();
+                bool success = Regex.IsMatch(word, @"^[a-zA-Z]+$");
                 if (success)
-                    dict.Add(word[0].ToString(), word);
+                    hash.insert(word);
                 else
-                    Console.WriteLine("Please enter words with letters only!");
-            };
-            ICollection keys = dict.Keys;
-            using (StreamWriter file = new StreamWriter(@"E:\STUDY\IT\Self-Study\C#\CS50\dictionary\dictionary.txt"))
-            {
-                foreach (string k in keys)
-                {
-                    file.WriteLine(k + ": " + dict[k]);
-                }
-            }                
-            
-            //
+                    break;                               
+            }
+            hash.print();
+            Console.ReadLine();
         }
     }
-    class Node
+    class hashNode
     {
-        public Node next = null;
-        public object data;
-        public Node(object val)
+        int _key;
+        string _word;
+        hashNode _next;
+        public hashNode(int key, string word)
         {
-            data = val;
+            _key = key;
+            _word = word;
+            _next = null;
+        }
+        public int key
+        {
+            get
+            {
+                return _key;
+            }
+            set
+            {
+                if (value >= 0 && value <= 25)
+                {
+                    _key = value;
+                }
+            }
+        }
+        public string word
+        {
+            get
+            {
+                return _word;
+            }
+            set
+            {
+                if (value != "")
+                {
+                    _word = value;
+                }
+            }
+        }
+        public hashNode next
+        {
+            get
+            {
+                return _next;
+            }
+            set
+            {
+                _next = value;
+            }
+        }
+    }
+    class FixedSizeHashTable
+    {
+        private int size;
+        hashNode[] hashTable;
+        public FixedSizeHashTable(int size)
+        {
+            this.size = size;
+            hashTable = new hashNode[size];
+            for (int i = 0; i < size; i++)
+                hashTable[i] = null;//First, set all indexes of hashTabe null value
+        }
+        public void insert(string word)
+        {
+            int key = word[0] - 97;
+            hashNode hn = new hashNode(key, word);
+            if (hashTable[key] == null)
+                hashTable[key] = hn;
+            else
+            {
+                hn.next = hashTable[key];
+                hashTable[key] = hn;
+            }
+        }
+        public void print()
+        {
+            hashNode current = null;
+            for (int i = 0; i < size; i++)
+            {
+                current = hashTable[i];
+                while (current != null)
+                {
+                    Console.Write(current.key + ": " + current.word + " -> ");
+                    current = current.next;
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
